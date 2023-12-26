3d7aea8dc2ce4a730c68c70106479494262b3e95
jonathan <jw-213685@rwu.de>
2023-12-26 17:53:57 +0100

    Versuch Liste zu erstellen

--- app/src/main/java/com/example/devicemanagement/Database.java
+++ app/src/main/java/com/example/devicemanagement/Database.java
@@ -4,21 +4,71 @@ import static android.content.ContentValues.TAG;
 
 import android.util.Log;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 public class Database {
+
     final static String dbUrl = "https://device-management-a2f26-default-rtdb.europe-west1.firebasedatabase.app/";
+    final static String pathStringDevices = "devices";
+    final static String pathStringObjects = "objects";
 
     public static void setUpDatabase() {
 
-        FirebaseDatabase database = FirebaseDatabase.getInstance(dbUrl);
-        DatabaseReference myRef = database.getReference("message");
+        DatabaseReference mDatabase = FirebaseDatabase.getInstance(dbUrl).getReference();
 
+        DatabaseReference devicesReference = mDatabase.child(pathStringDevices);
+        DatabaseReference objectsReference = mDatabase.child(pathStringObjects);
+
+/*
+        for (int i = 1; i <= 20; i++) {
+            Device d = new Device(i, "nameOfDevice" + i, "descriptionOfDevice" + i);
+            String id = "device" + d.getId();
+            mDatabase.child(pathStringDevices).child(id).setValue(d);
+        }
+*/
+
+        ValueEventListener valueEventListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                List<Device> devices = new ArrayList<>();
+                for (int i = 1; i <= 20; i++) {
+                    Device device = new Device(i, "nameOfDevice" + i, "descriptionOfDevice" + i);
+                    devices.add(device);
+                }
+                /* TODO: use this
+                **  deviceListLiveData.setValue(devices);
+                */
+
+                // Device device = dataSnapshot.getValue(Device.class);
+                // Log.d(TAG, "Value is: " + device);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // Getting Post failed, log a message
+                Log.w(TAG, "loadDevice:onCancelled", databaseError.toException());
+            }
+        };
+        mDatabase.addValueEventListener(valueEventListener);
+
+        /*
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("Device");
         myRef.setValue("Bye, World!");
+        Device device = new Device("name", "description", "serialNumber", new Date(), 1, "maintenanceStatus", "locationOfDevice", "operatingInstructions", "contactPartner");
+        myRef.setValue(device);
 
         myRef.addValueEventListener(new ValueEventListener() {
             @Override
@@ -35,6 +85,7 @@ public class Database {
                 Log.w(TAG, "Failed to read value.", error.toException());
             }
         });
-    }
 
+         */
+    }
 }
